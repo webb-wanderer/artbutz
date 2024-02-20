86693862bbd877f994a50a617777f223cfc5b368
webb-wanderer <140278726+webb-wanderer@users.noreply.github.com>
2024-02-19 21:44:09 -0800

    add navbar

--- _assets/js/footer-js.js
+++ _assets/js/footer-js.js
@@ -1,3 +1,3 @@
-$( "#nav-ab" ).load( "toc.htm" );
+$( "#nav-ab" ).load( "/toc.htm" );
 $( "#nav-hhp" ).load( "/_assets/includes/nav-hhp.htm" );
 $( "#footer" ).load( "/_assets/includes/footer.htm" );
