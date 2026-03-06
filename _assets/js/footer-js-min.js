eb09f3cab20cc98d781e2fa39963c080395710aa
webb-wanderer <140278726+webb-wanderer@users.noreply.github.com>
2026-03-06 13:41:32 -0800

    update document ready function

--- _assets/js/footer-js-min.js
+++ _assets/js/footer-js-min.js
@@ -1,1 +1,1 @@
-$("#nav-ab").load("/toc.htm"),$("#nav-hhp").load("/_assets/includes/nav-hhp.htm")
+$( document ).ready(function() { $( "#nav-ab" ).load( "/toc.htm" ); $( "#nav-hhp" ).load( "/_assets/includes/nav-hhp.htm" ); $( "footer" ).load( "/_assets/includes/footer.htm" ); });
--- _assets/js/footer-js.js
+++ _assets/js/footer-js.js
@@ -1,3 +1,5 @@
-$( "#nav-ab" ).load( "/toc.htm" );
-$( "#nav-hhp" ).load( "/_assets/includes/nav-hhp.htm" );
-$( "footer" ).load( "/_assets/includes/footer.htm" );
+$( document ).ready(function() {
+	$( "#nav-ab" ).load( "/toc.htm" );
+	$( "#nav-hhp" ).load( "/_assets/includes/nav-hhp.htm" );
+	$( "footer" ).load( "/_assets/includes/footer.htm" );
+});
